c8a9b191e07325db1600878bfee7165ce8641635
Shaunzy <68426146+lightstrike93@users.noreply.github.com>
2024-09-25 22:03:11 +0800

    Made a little test/start shell with basic inputs

---
+++ simple_shell.c
@@ -0,0 +1,93 @@
+/**
+ * @file simple_shell.c
+ * @brief A basic UNIX shell implementation for ICT374
+ * 
+ * This program implements a basic UNIX shell that reads user commands,
+ * executes them by forking child processes, and handles the `exit` command to
+ * terminate the shell. It also includes functionality to reclaim zombie
+ * processes.
+ * 
+ * @author Shaun Matthews & Louise Barjaktarevic 
+ * @date 25/09/2024
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_COMMAND_LENGTH 1024
+#define MAX_ARGS 100
+
+void read_command(char *command) {
+    printf("374 shell> ");
+    fflush(stdout);  
+    if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL) {
+        perror("fgets");
+        exit(1);
+    }
+
+    size_t len = strlen(command);
+    if (len > 0 && command[len - 1] == '\n') {
+        command[len - 1] = '\0';
+    }
+}
+
+void execute_command(char *command) {
+    char *args[MAX_ARGS];
+    int i = 0;
+
+    // simple tokenise
+    args[i] = strtok(command, " ");
+    while (args[i] != NULL) {
+        i++;
+        args[i] = strtok(NULL, " ");
+    }
+
+    if (strcmp(args[0], "exit") == 0) {
+        printf("Exiting shell...\n");
+        exit(0);
+    }
+
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        if (execvp(args[0], args) < 0) {
+            perror("execvp");
+            exit(1);
+        }
+    } else {
+        int status;
+        pid_t wpid;
+
+        do {
+            wpid = waitpid(-1, &status, WNOHANG);
+            if (wpid == -1) {
+                perror("waitpid");
+                exit(1);
+            }
+        } while (wpid > 0);
+    }
+}
+
+int main() {
+    char command[MAX_COMMAND_LENGTH];
+
+    while (1) {
+        // user input
+        read_command(command);
+
+        // Execute
+        if (strlen(command) > 0) {
+            execute_command(command);
+        }
+    }
+
+    return 0;
+}
