8bcd144a01633374f2a35cd2379673b1f71734e2
light <light@LOQBOOK>
2024-09-26 22:56:18 +0800

    fixed things

--- token.c
+++ token.c
@@ -4,32 +4,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int expand_wildcard(char *pattern, char *expanded_args[], int max_args)
 {
     glob_t glob_result;
     int i, count = 0;
 
- 
     memset(&glob_result, 0, sizeof(glob_result));
 
-    // wildcard expansion using glob
     if (glob(pattern, GLOB_TILDE, NULL, &glob_result) == 0)
     {
-    
         for (i = 0; i < glob_result.gl_pathc && count < max_args; i++)
         {
             expanded_args[count] = strdup(glob_result.gl_pathv[i]);
             count++;
         }
     }
+    else
+    {
+        // No matches found, return 0 to indicate no expansion
+        // TODO add print statement here
+        count = 0;
+    }
 
-
+    // free glob memory
     globfree(&glob_result);
 
     return count;
 }
-
 
 int tokenise(char line[], char *token[])
 {
@@ -40,25 +41,37 @@ int tokenise(char line[], char *token[])
     {
         if (i >= MAX_TOKENS)
         {
-            return -1;
+            return -1; // Too many tokens
         }
 
-        // token contains a wildcard character?
+        // check if token contains a wildcard character
         if (strchr(newToken, '*') || strchr(newToken, '?'))
         {
-            // expand the wildcard token and add expanded results to token array
-            i += expand_wildcard(newToken, &token[i], MAX_TOKENS - i);
+            int expanded = expand_wildcard(newToken, &token[i], MAX_TOKENS - i);
+            
+            // If no wildcard expansion was found, treat the token as-is
+            if (expanded == 0)
+            {
+                token[i] = newToken; 
+                i++;
+            }
+            else
+            {
+                i += expanded; 
+            }
         }
         else
         {
+            // Normal token, add to the token array
             token[i] = newToken;
             i++;
         }
 
+        // Move to the next token
         newToken = strtok(NULL, TOKEN_SEPARATORS);
     }
 
-    token[i] = NULL;
+    token[i] = NULL; 
 
     return i; 
 }
--- token.h
+++ token.h
@@ -1,7 +1,7 @@
 
 #define MAX_ARGS 100
 #define MAX_TOKENS 1000
-#define TOKEN_SEPARATORS " \t\n"
+#define TOKEN_SEPARATORS " \t\r\n"
 
 int tokenise(char line[], char *token[]);
 int expand_wildcard(char *pattern, char *expanded_args[], int max_args);
