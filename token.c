9bfec3f5ad5fba5adff4efbf690ca7343b842193
Shaunzy <68426146+lightstrike93@users.noreply.github.com>
2024-09-26 23:33:47 +0800

    fixed how ? works

--- token.c
+++ token.c
@@ -10,26 +10,29 @@ int expand_wildcard(char *pattern, char *expanded_args[], int max_args)
     glob_t glob_result;
     int i, count = 0;
 
- 
     memset(&glob_result, 0, sizeof(glob_result));
 
-    // wildcard expansion using glob
+    // Wildcard expansion using glob
     if (glob(pattern, GLOB_TILDE, NULL, &glob_result) == 0)
     {
-    
+        // Copy the expanded file paths into expanded_args
         for (i = 0; i < glob_result.gl_pathc && count < max_args; i++)
         {
             expanded_args[count] = strdup(glob_result.gl_pathv[i]);
             count++;
         }
     }
+    else
+    {
+        // No matches found, return 0 to indicate no expansion
+        count = 0;
+    }
 
-
+    // Free glob memory
     globfree(&glob_result);
 
     return count;
 }
-
 
 int tokenise(char line[], char *token[])
 {
@@ -40,25 +43,37 @@ int tokenise(char line[], char *token[])
     {
         if (i >= MAX_TOKENS)
         {
-            return -1;
+            return -1; // Too many tokens
         }
 
-        // token contains a wildcard character?
+        // Token contains a wildcard character?
         if (strchr(newToken, '*') || strchr(newToken, '?'))
         {
-            // expand the wildcard token and add expanded results to token array
-            i += expand_wildcard(newToken, &token[i], MAX_TOKENS - i);
+            int expanded = expand_wildcard(newToken, &token[i], MAX_TOKENS - i);
+            
+            // If no wildcard expansion was found, treat the token as-is
+            if (expanded == 0)
+            {
+                token[i] = newToken; // No expansion, add original token
+                i++;
+            }
+            else
+            {
+                i += expanded; // Increment i by the number of expanded tokens
+            }
         }
         else
         {
+            // Normal token, add to the token array
             token[i] = newToken;
             i++;
         }
 
+        // Move to the next token
         newToken = strtok(NULL, TOKEN_SEPARATORS);
     }
 
-    token[i] = NULL;
+    token[i] = NULL; // Null-terminate the token array
 
-    return i; 
+    return i; // Return the number of tokens found
 }
