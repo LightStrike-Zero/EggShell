4bf35e2735d560b92bd6adc1d8dcb350808e078b
lulu22 <louisebarjaktarevic0@gmail.com>
2024-09-29 00:36:40 +0800

    mess
    commit to the bit

--- history.h
+++ history.h
@@ -9,7 +9,8 @@
 extern char history[HISTORY_SIZE][MAX_COMMAND_LENGTH];
 extern int history_count;
 extern int history_index;
-
+void enable_raw_mode();   
+void disable_raw_mode(); 
 void show_history();
 void add_to_history(const char *command);
 void handle_history_navigation(char *command, int *index, const char *PS1);
