e8e9e45dedb3f882cb1d4490266985a1971fccdb
lulu22 <louisebarjaktarevic0@gmail.com>
2024-10-28 20:10:44 +0800

    got rid of ugly welcome msg

--- formatting.h
+++ formatting.h
@@ -4,13 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> 
-#include <sys/ioctl.h> // contains struct for winsize. to store the size of the users terminal. 
 
 
-// void get_terminal_size(int *rows, int *cols); // gets size of users terminal. 
-void welcome_message(int width); // displays welcome message, scaled to fit the users terminal size. 
-// why? because i want to display ascii art, and if its not scaled it looks like a dreadful mess of back slashes. 
+
+void welcome_message(); 
 
 
 #endif 
